0b8f4c934ef9129764f112a2dabed446d360c165
ELETROBUSINESS <eletroresponde@gmail.com>
2025-06-27 22:20:40 -0300

    login 1.12 27.06.25 22.20

--- public/assets/js/usersData.js
+++ public/assets/js/usersData.js
@@ -1,18 +1,18 @@
 // public/assets/js/usersData.js
 const users = [
   {
-    username: "45692327000100",
-    password: "senhaCNPJ",
+    username: "DTUDOVARIEDADES",
+    password: "251914",
     redirect: "/users/45692327000100/CD1/adm/nb/conta.html"
   },
   {
     username: "evelyn",
-    password: "senhaEvelyn",
+    password: "152510",
     redirect: "/users/45692327000100/CD1/colaborador/evelyn/conta.html"
   },
   {
     username: "ryan",
-    password: "senhaRyan",
+    password: "182011",
     redirect: "/users/45692327000100/CD1/colaborador/ryan/conta.html"
   }
 ];
