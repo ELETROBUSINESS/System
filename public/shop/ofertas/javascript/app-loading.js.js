637b9abaff26ceee620b9ee7083857663f218a4f
ELETROBUSINESS <eletroresponde@gmail.com>
2025-12-01 20:47:48 -0300

    optimization 01.12.25 20.47

--- public/shop/ofertas/javascript/app-loading.js.js
+++
@@ -1,9 +0,0 @@
-const pre_loading = document.querySelector("div.loading");
-
-function AnimationLoading() {
-    pre_loading.style.opacity = "0";
-
-    setTimeout(() => {
-        pre_loading.style.display = "none";
-    }, 500);
-}
